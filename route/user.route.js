f781e556fa3b559b7c6d5459861e337e4ea0feda
algamor59 <algamor59@gmail.com>
2025-05-07 12:54:46 +0200

    auth dans user route

--- route/user.route.js
+++ route/user.route.js
@@ -8,8 +8,8 @@ router.get('/:id', userController.getById);
 
 router.post('/', userController.create);
 
-router.put('/:id', userController.update);
-router.delete('/:id', userController.remove);
+router.put('/:id',auth, userController.update);
+router.delete('/:id',auth, userController.remove);
 
 router.put('/role/:userId/:roleId', auth("Admin"), userController.addRole);
 router.delete('/role/:userId/:roleId', auth("Admin"),userController.removeRole);
